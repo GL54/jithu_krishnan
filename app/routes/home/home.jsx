7edf324744333fdd7880103cd346234ad59504a5
gl54 <jchikku96@gmail.com>
2024-04-05 16:53:39 +0530

    first commit

---
+++ app/routes/home/home.jsx
@@ -0,0 +1,174 @@
+import yatch2Large from '~/assets/yatch-front.png';
+import yatch2Placeholder from '~/assets/gamestack-list-placeholder.jpg';
+import yatch2 from '~/assets/yatch-back.png';
+import yatchLarge from '~/assets/yatch-back.png';
+import yatchPlaceholder from '~/assets/gamestack-login-placeholder.jpg';
+import yatch from '~/assets/gamestack-login.jpg';
+import sliceTextureLarge from '~/assets/cianata.png';
+import sliceTexturePlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceTexture from '~/assets/cianata.png';
+import sprTextureLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from '~/assets/besoman.png';
+import { Footer } from '~/components/footer';
+import { baseMeta } from '~/utils/meta';
+import { Intro } from './intro';
+import { Profile } from './profile';
+import { ProjectSummary } from './project-summary';
+import { useEffect, useRef, useState } from 'react';
+import config from '~/config.json';
+import styles from './home.module.css';
+
+// Prefetch draco decoader wasm
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'Engineer + Developer',
+    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+  });
+};
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Besoman"
+        description="A website for a electronics solutions company"
+        buttonText="View website"
+        buttonLink="https://besoman.com"
+        model={{
+          type: 'laptop',
+          alt: 'besoman',
+          textures: [
+            {
+              srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2540w`,
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="YildizseYahat"
+        description="A website for renting exclusive Yatchs"
+        buttonText="View website"
+        buttonLink="https://yildizseyahat.com"
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: `${yatchLarge} 375w, ${yatchLarge} 750w`,
+              placeholder: yatchPlaceholder,
+            },
+            {
+              srcSet: `${yatch2Large} 375w, ${yatch2Large} 750w`,
+              placeholder: yatch2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="A Solutions company"
+        description="A web site for a solutions startup company called Cianata"
+        buttonText="View website"
+        buttonLink="https://cianata.com"
+        model={{
+          type: 'laptop',
+          alt: 'Cianata',
+          textures: [
+            {
+              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1920w`,
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
